165ea46bf5138c60aaeb0c4f209f782eb2e22181
qianqian <qianq5160@163.com>
2018-07-30 15:47:29 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# react_ssr
-react ssr
+### react 服务端渲染
+
+`
+参考链接
+https://segmentfault.com/a/1190000012998848
+`
---
+++ app/index.js
@@ -0,0 +1,4 @@
+import Demo from './main'
+import ReactDOM from 'react-dom'
+import React from 'react'
+ReactDOM.hydrate(<Demo />, document.getElementById('root'))
